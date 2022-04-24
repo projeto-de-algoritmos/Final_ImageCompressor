207479ef8d3c0c642baba245422671c22c261fc1
Eduardo Gurgel <51385738+EduardoGurgel@users.noreply.github.com>
2022-04-24 17:49:09 -0300

    Update tela.py

--- tela.py
+++ tela.py
@@ -22,3 +22,47 @@ def buscarArquivos():
 
 
     pathh.insert(tk.END, nomedoarquivo)
+
+
+
+def comprimirImagem():
+    
+    local = buscarArquivos.caminho
+    
+    h = 1
+    if h == 1:
+        
+        string_entrada = np.asarray(Image.open(local), np.uint8)
+        prnt = ("Local da imagem: " + local + "\n\n")
+
+        text.insert(tk.END, prnt)
+        shape = string_entrada.shape
+        a = string_entrada
+        print("Valores de entrada:", string_entrada)
+        string_entrada = str(string_entrada.tolist())
+    elif h == 2:
+        array = np.arange(0, 737280, 1, np.uint8)
+        string_entrada = np.reshape(array, (1024, 720))
+        print("valores de entrada:", string_entrada)
+        a = string_entrada
+        string_entrada = str(string_entrada.tolist())
+
+    else:
+        print("Imagem invalida")  # tipo de imagem selecionada
+
+    letras = []
+    apenas_letras = []
+    for letra in string_entrada:
+        if letra not in letras:
+            frequency = string_entrada.count(letra)  # Frequência de repetição de cada letra
+            letras.append(frequency)
+            letras.append(letra)
+            apenas_letras.append(letra)
+
+    nodes = []
+    while len(letras) > 0:
+        nodes.append(letras[0:2])
+        letras = letras[2:]  # Classificando de acordo com a frequência
+    nodes.sort()
+    huffman_tree = []
+    huffman_tree.append(nodes)  # Criando nós
